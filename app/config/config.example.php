4fdec30159e1d0a9b7fa93b365fc01397f716eda
LauraOrtega21 <laurapaolao2110@gmail.com>
2023-11-20 16:40:51 -0500

    Solucion de errores de configuracion

---
+++ app/config/config.example.php
@@ -0,0 +1,22 @@
+<?php
+// Definición de constantes para la URL y el nombre
+define('URL', '');
+define('NAME', '');
+
+// Definición de claves para encriptación
+define('KEY', 'mimamamemima');
+/* la master es la llave que encripta */
+define('MASTER', 'llaveparacodific');
+
+// Definición de detalles de la base de datos
+define('HOST', '');
+define('DB', '');
+define('USER', '');
+define('PASSWORD', '');
+define('CHARSET', '');
+
+// Definición de roles como un array asociativo
+define('ROLES', [
+  'admin' => 1,
+  'user' => 2
+]);
