bb5ec84613b04f4f0f5738c60f759cc527a4a0c2
PedroSYepes <yepeseyt2@gmail.com>
2023-12-13 10:50:35 -0500

    Comentarios finales

--- app/views/register.view.php
+++ app/views/register.view.php
@@ -1,94 +1,88 @@
 <div class="container">
+    <!-- Formulario para registrar datos -->
     <form id="login" class="login" action="<?= URL ?>/register/validate" method="POST" autocomplete="off">
         <h1 class="login__title">Registre sus datos</h1>
+        
+        <!-- Campo de entrada para el nombre -->
         <div>
             <input type="text" class="login__input" name="first_name" id="first_name" placeholder="Nombre">
             <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('name_error', $data['errors'])) { ?>
-                    <span class="login__error"><?= $data['errors']['name_error'] ?></span>
+            // Manejo de errores para el campo de nombre
+            if (isset($data['errors']) && array_key_exists('name_error', $data['errors'])) { ?>
+                <span class="login__error"><?= $data['errors']['name_error'] ?></span>
+            <?php } ?>
+            
             <?php
-                }
-            }
-            ?>
-            <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('user_duplicate', $data['errors'])) { ?>
-                    <span class="login__error"><?= $data['errors']['user_duplicate'] ?></span>
-            <?php
-                }
-            }
-            ?>
+            // Manejo de errores para el caso de duplicado de usuario
+            if (isset($data['errors']) && array_key_exists('user_duplicate', $data['errors'])) { ?>
+                <span class="login__error"><?= $data['errors']['user_duplicate'] ?></span>
+            <?php } ?>
         </div>
+        
+        <!-- Campo de entrada para los apellidos -->
         <div>
             <input type="text" class="login__input" name="last_name" id="last_name" placeholder="Apellidos">
             <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('last_error', $data['errors'])) { ?>
-                    <span class="login__error"><?= $data['errors']['last_error'] ?></span>
-            <?php
-                }
-            }
-            ?>
+            // Manejo de errores para el campo de apellidos
+            if (isset($data['errors']) && array_key_exists('last_error', $data['errors'])) { ?>
+                <span class="login__error"><?= $data['errors']['last_error'] ?></span>
+            <?php } ?>
         </div>
+        
+        <!-- Campo de entrada para el correo electrónico -->
         <div>
             <input type="text" class="login__input" id="email" name="email" placeholder="Correo electronico">
             <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('mail_error', $data['errors'])) { ?>
-                    <span class="login__error mail"><?= $data['errors']['mail_error'] ?></span>
+            // Manejo de errores para el campo de correo electrónico
+            if (isset($data['errors']) && array_key_exists('mail_error', $data['errors'])) { ?>
+                <span class="login__error mail"><?= $data['errors']['mail_error'] ?></span>
+            <?php } ?>
+            
             <?php
-                }
-            }
-            ?>
-            <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('mail_duplicate', $data['errors'])) { ?>
-                    <span class="login__error mail"><?= $data['errors']['mail_duplicate'] ?></span>
-            <?php
-                }
-            }
-            ?>
+            // Manejo de errores para el caso de duplicado de correo electrónico
+            if (isset($data['errors']) && array_key_exists('mail_duplicate', $data['errors'])) { ?>
+                <span class="login__error mail"><?= $data['errors']['mail_duplicate'] ?></span>
+            <?php } ?>
         </div>
+        
+        <!-- Campo de entrada para el número de teléfono -->
         <div>
             <input type="text" class="login__input" name="phone" id="phone" placeholder="Celular">
             <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('phone_error', $data['errors'])) { ?>
-                    <span class="login__error"><?= $data['errors']['phone_error'] ?></span>
-            <?php
-                }
-            }
-            ?>
+            // Manejo de errores para el campo de número de teléfono
+            if (isset($data['errors']) && array_key_exists('phone_error', $data['errors'])) { ?>
+                <span class="login__error"><?= $data['errors']['phone_error'] ?></span>
+            <?php } ?>
         </div>
+        
+        <!-- Campo de entrada para la contraseña -->
         <div>
             <input type="password" class="login__input" name="password" id="password" placeholder="Contraseña">
             <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('pass_error', $data['errors'])) { ?>
-                    <span class="login__error"><?= $data['errors']['pass_error'] ?></span>
-            <?php
-                }
-            }
-            ?>
+            // Manejo de errores para el campo de contraseña
+            if (isset($data['errors']) && array_key_exists('pass_error', $data['errors'])) { ?>
+                <span class="login__error"><?= $data['errors']['pass_error'] ?></span>
+            <?php } ?>
         </div>
+        
+        <!-- Campo de entrada para confirmar la contraseña -->
         <div>
             <input type="password" class="login__input" name="password_confirm" id="password_confirm" placeholder="Confirme su contraseña">
             <?php
-            if (isset($data['errors'])) {
-                if (array_key_exists('verify_error', $data['errors'])) { ?>
-                    <span class="login__error"><?= $data['errors']['verify_error'] ?></span>
-            <?php
-                }
-            }
-            ?>
+            // Manejo de errores para el campo de confirmación de contraseña
+            if (isset($data['errors']) && array_key_exists('verify_error', $data['errors'])) { ?>
+                <span class="login__error"><?= $data['errors']['verify_error'] ?></span>
+            <?php } ?>
         </div>
+        
+        <!-- Panel del formulario con botón de validación y enlace de inicio de sesión -->
         <div class="login__panel">
             <button class="login__btn" id="btnRegistrar">Validar datos</button>
             <a href="<?= URL ?>/login" class="login__link">Ya tengo usuario</a>
-        </div>        
+        </div>
     </form>
 </div>
 
+<!-- Scripts externos para la validación del formulario -->
 <script src="<?= URL ?>/assets/js/register.js"></script>
 <script src="<?= URL ?>/assets/js/valid.js"></script>
--- app/views/update.view.php
+++ app/views/update.view.php
@@ -1,4 +1,5 @@
 <div class="container">
+    <!-- Formulario para modificar la contraseña -->
     <form class="login" action="<?= URL ?>/login/updatepassword/<?= $data['data'] ?>" method="POST">
         <!-- Título del formulario -->
         <h1 class="login__title">Modificar contraseña</h1>
@@ -10,13 +11,9 @@
 
         <!-- Manejo de errores para el campo de contraseña -->
         <?php
-        if (isset($data['errors'])) {
-            if (array_key_exists('password_error', $data['errors'])) { ?>
-                <span class="login__error"><?= $data['errors']['password_error'] ?></span>
-        <?php
-            }
-        }
-        ?>
+        if (isset($data['errors']) && array_key_exists('password_error', $data['errors'])) { ?>
+            <span class="login__error"><?= $data['errors']['password_error'] ?></span>
+        <?php } ?>
 
         <!-- Campo de entrada para confirmar la nueva contraseña -->
         <div>
@@ -25,33 +22,21 @@
 
         <!-- Manejo de errores para el campo de confirmación de contraseña -->
         <?php
-        if (isset($data['errors'])) {
-            if (array_key_exists('confirm_password', $data['errors'])) { ?>
-                <span class="login__error"><?= $data['errors']['confirm_password'] ?></span>
-        <?php
-            }
-        }
-        ?>
+        if (isset($data['errors']) && array_key_exists('confirm_password', $data['errors'])) { ?>
+            <span class="login__error"><?= $data['errors']['confirm_password'] ?></span>
+        <?php } ?>
 
         <!-- Repetición del manejo de errores para el campo de contraseña (¿error tipográfico?) -->
         <?php
-        if (isset($data['errors'])) {
-            if (array_key_exists('password_error', $data['errors'])) { ?>
-                <span class="login__error"><?= $data['errors']['password_error'] ?></span>
-        <?php
-            }
-        }
-        ?>
+        if (isset($data['errors']) && array_key_exists('password_error', $data['errors'])) { ?>
+            <span class="login__error"><?= $data['errors']['password_error'] ?></span>
+        <?php } ?>
 
         <!-- Manejo de errores para el caso de caducidad de enlace (¿restablecimiento de contraseña?) -->
         <?php
-        if (isset($data['errors'])) {
-            if (array_key_exists('expire_error', $data['errors'])) { ?>
-                <span class="login__error"><?= $data['errors']['expire_error'] ?></span>
-        <?php
-            }
-        }
-        ?>
+        if (isset($data['errors']) && array_key_exists('expire_error', $data['errors'])) { ?>
+            <span class="login__error"><?= $data['errors']['expire_error'] ?></span>
+        <?php } ?>
 
         <!-- Panel del formulario con un campo oculto para el id y botón de validación -->
         <div class="login__panel">
@@ -59,12 +44,11 @@
             <button class="login__btn">Validar</button>
             
             <!-- Enlace para reenviar el enlace en caso de error de caducidad -->
-            <?php if (isset($data['errors'])) {
-                if (array_key_exists('expire_error', $data['errors'])) { ?>
-                    <a href="<?= URL ?>/login/forgetpassword" class="login__link">Reenviar link</a>
             <?php
-                }  
-            } ?>
+            if (isset($data['errors']) && array_key_exists('expire_error', $data['errors'])) { ?>
+                <a href="<?= URL ?>/login/forgetpassword" class="login__link">Reenviar link</a>
+            <?php } ?>
         </div>
     </form>
 </div>
+
